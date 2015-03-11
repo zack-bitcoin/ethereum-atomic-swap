e1bb0220fe3341eced124f19d226a5c7337f46f9
zack-bitcoin <zack.bitcoin@gmail.com>
2015-03-11 14:32:51 +0000

    spending requires signatures, database is based upon hash of secret instead of the addresses. fixed a bug in team_claim that stopped us from giving money to addr2

--- main_test.py
+++ main_test.py
@@ -18,16 +18,22 @@ def mk_acc(n):
 def mk_sig(hash, priv): return list(b.ecdsa_raw_sign(hash, priv))
 def mk_mk_sig(msghash): return (lambda p: mk_sig(msghash, p["priv"]))
 
+expiration=1000
+accs=map(mk_acc, [0, 1])
+secret_hash=int(hash_list([27]), 16)
 def test(c):
-    accs=map(mk_acc, [0, 1])
-    hash=hash_list([27])
-    c.spend(int(hash, 16), accs[0]["addr"], accs[1]["addr"], 1000)
-    c.reveal_claim(accs[0]["addr"], accs[1]["addr"], 27)
+    h=hash_list([secret_hash, expiration])
+    sig1=mk_sig(h, accs[0]["priv"])
+    sig2=mk_sig(h, accs[1]["priv"])
+    print("h: " +str(int(h, 16)))
+    c.spend(secret_hash, accs[0]["addr"], accs[1]["addr"], sig1, sig2, expiration)
+    c.reveal_claim(27)
     
 def test_team(c):
-    accs=map(mk_acc, [0, 1])
-    hash=hash_list([27])
-    c.spend(utils.big_endian_to_int(hash), accs[0]["addr"], accs[1]["addr"], 1000)
-    sig1=mk_sig(hash, accs[0]["priv"])
-    sig2=mk_sig(hash, accs[1]["priv"])
-    c.team_claim(accs[0]["addr"], accs[1]["addr"], sig1, sig2, 0)
+    h=hash_list([secret_hash, expiration])
+    sig1=mk_sig(h, accs[0]["priv"])
+    sig2=mk_sig(h, accs[1]["priv"])
+    c.spend(int(h, 16), accs[0]["addr"], accs[1]["addr"], expiration)
+    sig1=mk_sig(h, accs[0]["priv"])
+    sig2=mk_sig(h, accs[1]["priv"])
+    c.team_claim(int(hash, 16), sig1, sig2, 0)
