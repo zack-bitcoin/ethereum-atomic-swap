db3958564f7f26328695f2b19bacd9fdbe1a52ab
zack-bitcoin <zack.bitcoin@gmail.com>
2015-03-11 03:19:34 +0000

    switched sha3 -> sha256 for bitcoin compatibility

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ ethereum atomic cross-chain trading.
 
 based off of: https://en.bitcoin.it/wiki/Atomic_cross-chain_trading
 
+to test:
+
 ./run.py main
+
+About this contract:
+This is the ethereum-half of the atomic cross-chain trade. The bitcoin half looks like the wikipedia article. 
--- main_test.py
+++ main_test.py
@@ -9,7 +9,7 @@ def hash_list(l):
     y=[utils.zpad(x, 32) for x in y]
     y=''.join(y)
     #save for pretty print z="".join("{:02x}".format(ord(c)) for c in y)
-    return utils.sha3(y)
+    return b.sha256(y)
 def mk_acc(n):
     out={"priv":utils.sha3("brainwallet"+str(n))}
     out["pub"]=b.privtopub(out["priv"])
@@ -21,7 +21,7 @@ def mk_mk_sig(msghash): return (lambda p: mk_sig(msghash, p["priv"]))
 def test(c):
     accs=map(mk_acc, [0, 1])
     hash=hash_list([27])
-    c.spend(utils.big_endian_to_int(hash), accs[0]["addr"], accs[1]["addr"], 1000)
+    c.spend(int(hash, 16), accs[0]["addr"], accs[1]["addr"], 1000)
     c.reveal_claim(accs[0]["addr"], accs[1]["addr"], 27)
     
 def test_team(c):
